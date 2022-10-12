fe4ded2ff00743de7c6874c804cd1bf8e6446806
Rad Eshghi <reshghi@myseneca.ca>
2022-10-11 20:18:09 -0400

    EC2 URL updated

--- src/api.js
+++ src/api.js
@@ -3,7 +3,8 @@
 import { getUser } from "./auth";
 
 // fragments microservice API, defaults to localhost:8080
-const apiUrl = process.env.API_URL || 'http://localhost:8080';
+//const apiUrl = process.env.API_URL || 'http://localhost:8080';
+const apiUrl = process.env.API_URL || 'http://ec2-52-90-231-94.compute-1.amazonaws.com:8080';
 
 /**
  * Given an authenticated user, request all fragments for this user from the
